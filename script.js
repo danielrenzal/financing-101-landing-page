c8a08cb2fd96a0baa5ff58bb69460c4d6fd74237
danielrenzal <57828719+danielrenzal@users.noreply.github.com>
2022-09-15 15:25:03 +0800

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //accordion
+    //accordion 1
     const methods = document.querySelectorAll('.method-accordion .method');
 
     for(let i=0; i<methods.length; i++){
         methods[i].addEventListener('click', function(){
             this.children[1].classList.toggle('display-body');
             this.children[0].children[2].classList.toggle('rotate-arrow');
+        })
+    }
+
+    //accordion 2
+    const questions = document.querySelectorAll('.faqs-accordion .question');
+
+    for(let i=0; i<questions.length; i++){
+        questions[i].addEventListener('click', function(){
+            this.children[1].classList.toggle('display-body');
+            this.children[0].children[1].classList.toggle('rotate-arrow');
         })
     }
 });
