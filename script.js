49af27aa67ce30911732ea611ff22f59471c2182
danielrenzal <57828719+danielrenzal@users.noreply.github.com>
2022-09-16 15:29:52 +0800

    code changes
    Replaced all classes with dash with underscore

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     //accordion 1
-    const methods = document.querySelectorAll('.method-accordion .method');
+    const methods = document.querySelectorAll('.method_accordion .method');
 
     for(let i=0; i<methods.length; i++){
         methods[i].addEventListener('click', function(){
-            this.children[1].classList.toggle('display-body');
+            this.children[1].classList.toggle('display_body');
             this.children[0].children[2].classList.toggle('rotate-arrow');
         })
     }
 
     //accordion 2
-    const questions = document.querySelectorAll('.faqs-accordion .question');
+    const questions = document.querySelectorAll('.faqs_accordion .question');
 
     for(let i=0; i<questions.length; i++){
         questions[i].addEventListener('click', function(){
-            this.children[1].classList.toggle('display-body');
+            this.children[1].classList.toggle('display_body');
             this.children[0].children[1].classList.toggle('rotate-arrow');
         })
     }
